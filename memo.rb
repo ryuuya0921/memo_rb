241f5cad8cdc644d2a5e8fd47acbf859cacbf773
ryuuya <ryuuya0921@gmail.com>
2024-01-30 06:19:50 +0900

    修正

--- memo.rb
+++ memo.rb
@@ -10,12 +10,35 @@ if memo_type == 1
   # chompはgetsで取得した文字列から改行文字を除去できる
   
   puts "メモした内容を記入してください"
-  puts "完了したら Ctrl + Dをおします"
+  puts "完了したら Ctrl + Dを押します"
   
-  input_memo = STDIN.read
+  input_memo = STDIN.read # 全ての入力を一度で読み込む。複数行の入力を一度に処理することができる。
   memo = input_memo.chomp
 
   CSV.open("#{file_name}.csv","w") do |csv|
+  csv.puts ["#{memo}"]
+  end
+
+elsif memo_type == 2
+
+  puts "既存のメモを編集します。拡張子を除いた既存ファイル名を入力してください"
+  name = gets.chomp
+
+  require 'csv'
+
+  memo_list = CSV.read("#{name}.csv")
+  #CSV形式のファイルからデータを読み込むためのメソッド
+  #{name}.csv は、文字列内で変数の値を展開するためのRubyの式
+
+  p memo_list
+
+  puts "編集したい内容を入力してください"
+  puts "終了後、Ctrl + Dを押します"
+
+  input_memo = STDIN.read # 全ての入力を一度で読み込む。複数行の入力を一度に処理することができる。
+  memo = input_memo.chomp
+  
+  CSV.open("#{memo}.csv","a") do |csv| #引数の"a"は、ファイルを追記モードで開くことを示している。
   csv.puts ["#{memo}"]
   end
 end
