ea30ed46a2f344aa4efa7f246f0f01628ff678ff
ryuuya <ryuuya0921@gmail.com>
2024-01-30 06:49:42 +0900

    完成

--- memo.rb
+++ memo.rb
@@ -1,47 +1,31 @@
-require 'csv' # CSVファイルを扱うためのライブラリを読み込んでいる
+require 'csv'
 
 puts "1(新規でメモを作成) 2(既存のメモを編集する)"
-
-memo_type = gets.to_i # ユーザーの入力値を取得し、数字へ変換している
+memo_type = gets.to_i
 
 if memo_type == 1
   puts "拡張子を除いたファイルを入力してください"
   file_name = gets.chomp
-  # chompはgetsで取得した文字列から改行文字を除去できる
-  
+
   puts "メモした内容を記入してください"
   puts "完了したら Ctrl + Dを押します"
-  
-  input_memo = STDIN.read # 全ての入力を一度で読み込む。複数行の入力を一度に処理することができる。
+  input_memo = STDIN.read
   memo = input_memo.chomp
 
   CSV.open("#{file_name}.csv","w") do |csv|
-  csv.puts ["#{memo}"]
+    csv.puts ["#{memo}"]
   end
-
 elsif memo_type == 2
-
   puts "既存のメモを編集します。拡張子を除いた既存ファイル名を入力してください"
   name = gets.chomp
 
-  require 'csv'
-
-  memo_list = CSV.read("#{name}.csv")
-  #CSV形式のファイルからデータを読み込むためのメソッド
-  #{name}.csv は、文字列内で変数の値を展開するためのRubyの式
-
-  p memo_list
-
+  # 既存のファイルの内容を削除して新しい内容を書き込む
   puts "編集したい内容を入力してください"
   puts "終了後、Ctrl + Dを押します"
-
-  input_memo = STDIN.read # 全ての入力を一度で読み込む。複数行の入力を一度に処理することができる。
+  input_memo = STDIN.read
   memo = input_memo.chomp
-  
-  CSV.open("#{name}.csv","a") do |csv| #引数の"a"は、ファイルを追記モードで開くことを示している。
-  csv.puts ["#{memo}"]
+
+  CSV.open("#{name}.csv","w") do |csv|
+    csv.puts ["#{memo}"]
   end
 end
-
-
-
